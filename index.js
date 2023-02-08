53c94ef2d00b8e2f4482f881f6c036baf233e304
tom-2810 <tom.poortman@hva.nl>
2023-02-08 11:31:18 +0100

    WHOIS API connectie

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // Importeer express uit de node_modules map
-import express from 'express'
+import express, { response } from 'express'
 
 // Maak een nieuwe express app aan
 const app = express()
+
+const url = 'https://whois.fdnd.nl/api/v1/member/tom-poortman';
+const data = await fetch(url).then((response) => response.json());
+
+console.log(data);
 
 // Stel ejs in als template engine en geef de 'views' map door
 app.set('view engine', 'ejs')
@@ -14,7 +19,7 @@ app.use(express.static('public'))
 // Maak een route voor de index
 app.get('/', function (req, res) {
   // res.send('Hello World!')
-  res.render('index')
+  res.render('index',data)
 })
 
 // Stel het poortnummer in waar express op gaat luisteren
